f9e83cb0219d3f87d592fad87534ecbcd7e1415f
Prosanto7 <prosantodeb7@gmail.com>
2023-06-08 18:47:10 +0600

    Header, Footer, Navbar Added

---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+        <footer class="mt-5" style="background-color: #ccccb3">
+            <div class="container section">
+                <div class="row">
+                    <div class="mx-auto text-center">
+                        <a class="d-inline-block pb-2 pt-2" href="index.php">
+                        <img loading="prelaod" decoding="async" class="img-fluid" src="images/logo.png" alt="Rental Solutions" width="300px">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="copyright bg-dark content">Designed &amp; Developed By <a href="https://github.com/Prosanto7">Prosanto Deb</a></div>
+        </footer>
+
+
+        <!-- # JS Plugins -->
+        <script src="plugins/jquery/jquery.min.js"></script>
+        <script src="plugins/bootstrap/bootstrap.min.js"></script>
+
+        <!-- Main Script -->
+        <script src="js/script.js"></script>
+
+    </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html lang="en-us">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Rental Solutions</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="A Content Management System (CMS) project that helps to rent your next house.">
+        <meta name="author" content="Prosanto Deb">
+        <meta name="keywords" content="HTML, CSS, JavaScript, CMS, PHP">
+        <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+        <link rel="icon" href="images/favicon.png" type="image/x-icon">
+    
+        <!-- # Google Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Neuton:wght@700&family=Work+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+        <!-- # CSS Plugins -->
+        <link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css">
+
+        <!-- # Main Style Sheet -->
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body>
