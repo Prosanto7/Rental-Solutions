18fcb21a8f077f6df4fe61c67d72c64b6d36ff51
Prosanto7 <prosantodeb7@gmail.com>
2023-06-12 14:23:47 +0600

    AdminDashboard: Added admin navigation icons

---
+++ admin/includes/navigation.php
@@ -0,0 +1,43 @@
+<div class="container">
+    <aside>
+        <div class="top">
+            <div class="logo">
+                <img src="images/logo.png" alt="Rental Solutions">
+            </div>
+            <div class="close" id="close-btn">
+                <span class="material-symbols-sharp">close</span>
+            </div>
+        </div>
+
+        <div class="sidebar">
+            <a href="">
+                <span class="material-symbols-sharp">grid_view</span>
+                <h3>Dashboard</h3>
+            </a>
+            <a href="">
+                <span class="material-symbols-sharp">post</span>
+                <h3>Posts</h3>
+            </a>
+            <a href="">
+                <span class="material-symbols-sharp">category</span>
+                <h3>Categories</h3>
+            </a>
+            <a href="">
+                <span class="material-symbols-sharp">sell</span>
+                <h3>Tags</h3>
+            </a>
+            <a href="">
+                <span class="material-symbols-sharp">group</span>
+                <h3>Users</h3>
+            </a>
+            <a href="">
+                <span class="material-symbols-sharp">mail</span>
+                <h3>Messages</h3>
+            </a>
+            <a href="">
+                <span class="material-symbols-sharp">logout</span>
+                <h3>Log Out</h3>
+            </a>
+        </div>
+    </aside>
+</div>
