6bb5fc94175c5e1b0d6a79b6f9051f15ea83511a
Prosanto7 <prosantodeb7@gmail.com>
2023-06-12 13:03:14 +0600

    AdminDashboard: Added header, footer, functions and  navigation file

---
+++ admin/includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Admin Panel</title>
+        
+        <!-- CSS Stylesheet -->
+        <link rel="stylesheet" href="css/style.css">
+
+        <!-- Material CDN -->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    </head>
+
+    <body>
+
+    
---
+++ admin/index.php
@@ -0,0 +1,9 @@
+<?php
+    require_once('includes/header.php');
+    require_once('includes/functions.php');
+    require_once('includes/navigation.php');
+?>
+
+<?php    
+    require_once('includes/footer.php');
+?>
