6bce1a909ee529149a8eec500819fde651960912
Prosanto7 <prosantodeb7@gmail.com>
2023-06-13 16:32:30 +0600

    AdminDashboard: Added theme toggle option

--- admin/js/script.js
+++ admin/js/script.js
@@ -1,8 +1,7 @@
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector('#menu-btn');
 const closeBtn = document.querySelector("#close-btn");
-
-console.log(menuBtn);
+const themeToggler = document.querySelector(".theme-toggler");
 
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block';
@@ -10,4 +9,11 @@ menuBtn.addEventListener('click', () => {
 
 closeBtn.addEventListener('click', () => {
     sideMenu.style.display = 'none';
+})
+
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
 })
