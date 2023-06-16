113c593a51652fd1dd5b8b305885e2d213ad34d8
Prosanto7 <prosantodeb7@gmail.com>
2023-06-16 11:03:29 +0600

    UserRegistration: Updated registration form

---
+++ views/registration.php
@@ -0,0 +1,29 @@
+<main>
+    <div class="container">
+        <h2 class="text-center mt-5">User Registration</h2>
+        <form class="form" action="">
+            <label class="form-label mt-3">Name</label>
+            <input type="text" class="form-control" placeholder="Enter name...">
+
+            <label class="form-label mt-3">Email Address</label>
+            <input type="email" class="form-control" placeholder="Enter email address...">
+
+            <label class="form-label mt-3">Date of birth</label>
+            <input type="date" class="form-control" placeholder="Enter date of birth...">
+
+            <label class="form-label mt-3">Present address</label>
+            <input type="text" class="form-control" placeholder="Enter present address...">
+
+            <label class="form-label mt-3">Contact Number</label>
+            <input type="text" class="form-control" placeholder="Enter contact number...">
+
+            <label class="form-label mt-3">Username</label>
+            <input type="text" class="form-control" placeholder="Enter username...">
+
+            <label class="form-label mt-3">Password</label>
+            <input type="password" class="form-control" placeholder="Enter password...">
+            
+            <input type="submit" value="Login" class="btn btn-primary mt-3">
+        </form>
+    </div>
+</main>
