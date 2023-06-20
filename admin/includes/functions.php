f8be84105168740ecfc22ada7e793378a15aae47
Prosanto7 <prosantodeb7@gmail.com>
2023-06-20 14:59:58 +0600

    UserRegistration: Added markup to show list of users from Admin

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -8,6 +8,8 @@
             include("posts.php");
         } else if ($page == "tags") {
             include("tags.php");
+        } else if ($page == "users") {
+            include("users.php");
         } else {
             include("main-section.php");
         }
---
+++ admin/includes/users.php
@@ -0,0 +1,25 @@
+<main>
+<div class="styledTable">
+        <h2>All Users</h2>
+        <table id="userTable">
+            <thead>
+                <tr>
+                    <th>User ID</th>
+                    <th>Username</th>
+                    <th>User First Name</th>
+                    <th>User Last Name</th>
+                    <th>User Email</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody id="userTableBody">
+
+            </tbody>
+        </table>
+        <script>
+            $(document).ready(function() {
+                
+            });
+        </script>
+    </div>
+</main>
