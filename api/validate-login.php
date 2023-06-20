e9bba44cf98a7d4ebc8aedf5c8bb5b2e8dac3c46
Prosanto7 <prosantodeb7@gmail.com>
2023-06-20 18:35:19 +0600

    Login: Implemented login feature

--- api/validate-login.php
+++ api/validate-login.php
@@ -1,7 +1,7 @@
 <?php 
     require_once('../includes/functions.php');
 
-    $loginData = executeQuery("SELECT * FROM users WHERE username = '" . $_POST["username"] . "' AND password = '" . $_POST["password"] ."'");
+    $loginData = executeQuery("SELECT * FROM users WHERE username = '" . $_POST["user_name"] . "' AND user_password = '" . $_POST["user_password"] ."'");
    
     if ($loginData->num_rows == 1) {
         echo 1;
--- views/login.php
+++ views/login.php
@@ -1,22 +1,32 @@
 <main>
     <div class="container">
         <h2 class="text-center mt-5">Login</h2>
-        <form class="form" autocomplete="off">
+        <form class="form" autocomplete="off" id="loginForm">
             <label class="form-label mt-3">Username</label>
-            <input type="text" class="form-control" placeholder="Enter username...">
+            <input type="text" class="form-control" placeholder="Enter username..." name="user_name">
             <label class="form-label mt-3">Password</label>
-            <input type="password" class="form-control" placeholder="Enter password...">
-            <input type="submit" value="Login" class="btn btn-primary mt-3" id="login">
+            <input type="password" class="form-control" placeholder="Enter password..." name="user_password">
+            <input type="submit" value="Login" class="btn btn-primary mt-3" id="loginButton">
         </form>
         <script>
             $(document).ready(function() {
-                $(document).on("click", "#postCommentButton", function(e) {
+                $(document).on("click", "#loginButton", function(e) {
+                    e.preventDefault();
+
+                    var form = new FormData(document.getElementById('loginForm'));
+
                     $.ajax({
                         url: 'api/validate-login.php',
                         type: 'post',
                         data: form,
+                        processData: false,
+                        contentType: false,
                         success: function(data) {
-                            
+                            if (data == 1) {
+                                showAlertMessage("Success", "Welcome to Rental Solutions");
+                            } else {
+                                showAlertMessage("Error", "Please enter valid user name and password...");
+                            }
                         }
                     });
                 });
