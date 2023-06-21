168111ac1cd8ce0f7cee500e31ef4b9a892aaaf6
Prosanto7 <prosantodeb7@gmail.com>
2023-06-21 17:41:48 +0600

    AdminBackend: Updated list of posts from admin

--- admin/includes/posts.php
+++ admin/includes/posts.php
@@ -1,13 +1,13 @@
 <style>
-    .container{
+    .container {
         grid-template-columns: 15rem auto 8rem;
     }
 </style>
 
 <main>
     <div class="styledTable posts">
-        <h2>All Posts</h2>
-        <table id="postTable">
+        <h2>Pending Posts</h2>
+        <table id="pendingPostTable">
             <thead>
                 <tr>
                     <th>Post ID</th>
@@ -20,14 +20,61 @@
                     <th>See Details</th>
                 </tr>
             </thead>
-            <tbody id="postTableBody">
-                
+            <tbody id="pendingPostTableBody">
+
             </tbody>
         </table>
         <script>
             $(document).ready(function() {
-                populate('api/fetch-post.php', 'post', '#postTableBody');
-                deleteRow('api/delete-post.php', 'api/fetch-post.php', 'post', '#postTableBody');
+                populate('api/fetch-post.php?status=draft', 'post', '#pendingPostTableBody');
+                deleteRow('api/delete-post.php', 'api/fetch-post.php?status=draft', 'post', '#pendingPostTableBody');
+            });
+        </script>
+    </div>
+
+    <div class="styledTable posts">
+        <h2>Approved Posts</h2>
+        <table id="approvedPostTable">
+            <thead>
+                <tr>
+                    <th>Post ID</th>
+                    <th>Date & Time</th>
+                    <th>Posted By</th>
+                    <th>Post Title</th>
+                    <th>Post Category</th>
+                    <th>Action</th>
+                    <th>See Details</th>
+                </tr>
+            </thead>
+            <tbody id="approvedPostTableBody">
+
+            </tbody>
+        </table>
+        <script>
+            $(document).ready(function() {
+                populate('api/fetch-post.php?status=approved', 'post', '#approvedPostTableBody');
+                deleteRow('api/delete-post.php', 'api/fetch-post.php?status=approved', 'post', '#approvedPostTableBody');
+                $(document).on("click", "#approve", function() {
+                    var id = $(this).data("id");
+
+                    $.ajax({
+                        url: 'api/approve-post.php',
+                        type: 'post',
+                        data: {
+                            id: id
+                        },
+                        success: function(data) {
+                            populate('api/fetch-post.php?status=approved', 'post', '#approvedPostTableBody');
+                            populate('api/fetch-post.php?status=draft', 'post', '#pendingPostTableBody');
+
+                            if (data == 1) {
+                                showAlertMessage("Success", "Post approved successfully...");
+                            } else {
+                                showAlertMessage("Error", "Post could not be approved.");
+                            }
+                        }
+                    });
+                })
             });
         </script>
     </div>
