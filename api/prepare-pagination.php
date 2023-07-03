5f20beeff0789c65ccd2c5a07ee40ed6888955c1
Prosanto7 <prosantodeb7@gmail.com>
2023-07-03 14:37:22 +0600

    Pagination: Updated number of pages for small and large devices

--- api/prepare-pagination.php
+++ api/prepare-pagination.php
@@ -5,39 +5,47 @@
     
     
     $postCount = $_POST["count"];
+   
 
     if ($_POST["width"] > 700) {
-       
         if ($postCount < 10) {
-            $data .= "<li class='page-item'><a class='page-link' href='#'>{$postCount}</a></li>";
             for ($i = 1; $i <= $postCount; $i++) {
-                if ((isset($_GET["no"]) && $i == $_GET["no"]) || (!isset($_GET["no"]) && $i == 1)) {
-                    $data .= "<li class='page-item active'><a class='page-link' href='index.php?page=home&no={$i}'>{$i}</a></li>";
-                } else {
-                    $data .= "<li class='page-item'><a class='page-link' href='index.php?page=home&no={$i}'>{$i}</a></li>";
-                }
-            }
+                $data .= printLink($i, $data);
+            }  
         } else {
             for ($i = 1; $i <= 4; $i++) {
-                if ((isset($_GET["no"]) && $i == $_GET["no"]) || (!isset($_GET["no"]) && $i == 1)) {
-                    $data .= "<li class='page-item active'><a class='page-link' href='index.php?page=home&no={$i}'>{$i}</a></li>";
-                } else {
-                    $data .= "<li class='page-item'><a class='page-link' href='index.php?page=home&no={$i}'>{$i}</a></li>";
-                }
+                $data .= printLink($i, $data);
             }
 
             $data .= "<li class='page-item'><a class='page-link'>..</a></li>";
 
 
             for ($i = $postCount - 4; $i <= $postCount; $i++) {
-                if ((isset($_GET["no"]) && $i == $_GET["no"]) || (!isset($_GET["no"]) && $i == 1)) {
-                    $data .= "<li class='page-item active'><a class='page-link' href='index.php?page=home&no={$i}'>{$i}</a></li>";
-                } else {
-                    $data .= "<li class='page-item'><a class='page-link' href='index.php?page=home&no={$i}'>{$i}</a></li>";
-                }
+                $data .= printLink($i, $data);
+            }
+           
+        }
+    } else {
+        if ($postCount < 5) {
+            for ($i = 1; $i <= $postCount; $i++) {
+                $data .= printLink($i, $data);
+            }  
+        } else {
+            for ($i = 1; $i <= 5; $i++) {
+                $data .= printLink($i, $data);
             }
         }
     }
     $data .= "<li class='page-item'><a class='page-link' href='#'>Next</a></li>";
     echo $data;
+?>
+
+<?php 
+    function printLink($i) {
+        if ($i == $_GET["no"]) {
+            return "<li class='page-item active'><a class='page-link' href='index.php?page=home&no={$i}'>{$i}</a></li>";
+        } else {
+            return "<li class='page-item'><a class='page-link' href='index.php?page=home&no={$i}'>{$i}</a></li>";
+        }
+    }
 ?>
